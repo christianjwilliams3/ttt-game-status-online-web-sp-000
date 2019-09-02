968e8b0f5b2c1d9eb6ea112ae4d6ba4500b844f2
christian williams <christian.williamsj@gmail.com>
2019-09-02 06:56:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,4 +16,8 @@ def WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-  WIN_COMBINATIONS.boars {won
+  WIN_COMBINATIONS.boars {win_combo}
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
