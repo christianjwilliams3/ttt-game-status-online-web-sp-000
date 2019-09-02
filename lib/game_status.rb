d29bd887752699f39f203a6e2bacc39576e1aff5
christian williams <christian.williamsj@gmail.com>
2019-09-02 06:58:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,4 +21,8 @@ def won?(board)
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
   
-   position_1 = board[win_index_1]
+   position_1 = board[win_index_0]
+   position_2 = board[win_index_1]
+   position_3 = board[win_index_2]
+   
+ end 
