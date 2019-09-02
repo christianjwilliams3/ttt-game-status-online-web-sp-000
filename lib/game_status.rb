6cb0ccaf673c1da3baa1d6f8e8704be1d4b9dfe2
christian williams <christian.williamsj@gmail.com>
2019-09-02 07:39:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,7 +54,19 @@ def over?(board)
   end
 end 
 
-def winner
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
   
   
   
